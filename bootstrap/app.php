9fc93d9e47dc598b7b07d138f3bc8b64cc313333
Johan Vester <johan.vester@gmail.com>
2015-12-13 03:13:37 +0100

    Added temporary unrestricted CORS header

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -60,13 +60,14 @@ $app->singleton(
 |
 */
 
-// $app->middleware([
+$app->middleware([
 //     // Illuminate\Cookie\Middleware\EncryptCookies::class,
 //     // Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
 //     // Illuminate\Session\Middleware\StartSession::class,
 //     // Illuminate\View\Middleware\ShareErrorsFromSession::class,
 //     // Laravel\Lumen\Http\Middleware\VerifyCsrfToken::class,
-// ]);
+    App\Http\Middleware\CorsMiddleware::class,
+]);
 
 $app->routeMiddleware([
     'jwt.auth'    => Tymon\JWTAuth\Middleware\GetUserFromToken::class,
